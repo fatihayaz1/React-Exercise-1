03a2ba99ca3e82b80e36ee088c869326a2a2b576
Fatih Ayaz <ayazfatih61@gmail.com>
2022-10-29 00:40:20 +0300

    anlatım eklendi.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-import axios from "axios"
+import axios from "axios" //axios modulünü eklemiş olduk. baştan kurmak için terminale "npm i axios"
 
 
 
 
-async function getData (number) {
+async function getData (number) { //basit bir fonksiyon belirledik. başına istendiği üzere async yazdık.
+    //async yazmamızın sebebi verilen bilgileri ve işlemleri sırasıyla yapmasını istememiz.
     const user = await axios(`https://jsonplaceholder.typicode.com/users/${number}`)
+    // user değişkenine verilen linki ekledik. linkin sonunda yer alan sayıyı biz vermek için number 
+    //değişkenini yazdık ${} ile. 
+    //axios ekleyerek bilgiyi çektik verilen linkten.
+    //başına await ekleyerek verilen linkten gelen bilgiyi beklemesini söyledik.
     const post = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${number}`)
+    //yukarıda yaptığımız işlemin aynısını uyguladık.
     let response = {
         ...user,
         ...post
-    }
+    } //user değişkenini ve post değişkenini tek bir objeye atadık ve yazdırdık.
     console.log(response)
 }
-export {getData}
+export {getData} // fonksiyonun başka bir sayfada kullanılabilmesi için export ettik.
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import { getData } from "./app.js";
+import { getData } from "./app.js"; // app.js sayfasındaki getData fonksiyonu çektik.
 
-const number = 1
+const number = 1  // fonksiyonda bulunan number parametresine bir sayı atadık.
+//atama yapmayıp direkt olarak getData(1)'de yazılabilirdi.
 getData(number)
+
+//ayrıca package.json dosyasına "type": "module", eklemeyi unutmamanız gerekiyor.
